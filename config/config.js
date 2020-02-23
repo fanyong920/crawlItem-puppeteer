1b89f4a4e20367e3df9590736b022323eace35fd
fanyong920 <1023079644@qq.com>
2020-02-23 22:18:57 +0800

    测试

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 var appConfig ={
     MAX_WSE:3,
-    parallelCrawler:9,
+    parallelCrawler:4,
     parallelBrowser:1,
-    maxCrawlerCount:100
+    maxCrawlerCount:200
 }
 
 module.exports =  appConfig;
