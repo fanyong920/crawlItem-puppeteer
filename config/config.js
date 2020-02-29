86dc2c79da0fc13a0298a07efc789039538d8528
fanyong920 <1023079644@qq.com>
2020-02-29 17:42:09 +0800

    内容：config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const os = require('os');
 const log4js = require('../public/javascripts/util/log4jUtil')
 const infoLog = log4js.getLogger('infoLog');
-const num = (os.cpus().length)*2 + 1;
+const num = os.cpus().length*2 + 1;
 infoLog.log("系统将配置爬网页的并发数："+num);
 var appConfig ={
     MAX_BROWSER:3,//大考的浏览器实例
@@ -11,7 +11,7 @@ var appConfig ={
     {
         headless: true,
         args: [
-            '--no-sandbox', '--disable-setuid-sandbox', '–-disable-gpu', '--use-gl=swiftshader', '--disable-gl-drawing-for-tests', '--blik-settings=doHtmlPreloadScanning=false'
+            '--no-sandbox', '--disable-setuid-sandbox'/* , '–-disable-gpu', '--use-gl=swiftshader', '--disable-gl-drawing-for-tests', '--blik-settings=doHtmlPreloadScanning=false' */
         ],
         ignoreDefaultArgs: ["--enable-automation"]
         // executablePath:"C:\\Users\\10230\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe"
