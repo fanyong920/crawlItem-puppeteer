1e10ed7390fb246831c27bebad68cf8661df7c3b
fanyong920 <1023079644@qq.com>
2020-02-29 17:33:01 +0800

    内容：在pool和array之间比较测试

--- public/javascripts/browserManager.js
+++ public/javascripts/browserManager.js
@@ -1,98 +1,27 @@
 const puppeteer = require('puppeteer');
-const genericPool = require('generic-pool');
 const appConfig = require('../../config/config');
 const log4js = require('./util/log4jUtil');
 const dateUtil = require('./util/dateUtil');
 const schedule = require('node-schedule');
 
 const infoLog = log4js.getLogger('infoLog');
-const errorLog = log4js.getLogger('errorLog');
 
 const launchOptions = appConfig.launchOptions;
 
 const MAX_BROWSER = appConfig.MAX_BROWSER == 0 ? 3 : appConfig.MAX_BROWSER;  //启动几个浏览器 
-
-const factory = {
-    create: () =>
-        puppeteer.launch(launchOptions).then(browser => {
-            // 创建一个 puppeteer 实例 ，并且初始化使用次数为 0
+const BROWSER_ARRAY = [];
+init();
+function init(){
+	(async () => {
+		for(var i = 0;i < MAX_BROWSER;i++){
+            let browser = await puppeteer.launch(launchOptions);
             infoLog.info("browser create",browser.wsEndpoint());
             browser.useCount = 0
-            return browser
-        }),
-    destroy: browser => {
-        setTimeout(function(){
-            infoLog.info("browser close",browser.wsEndpoint());
-            browser.close()
-        },2000)
-        
-    }
-    // validate: browser => {
-    //     // 执行一次自定义校验，并且校验校验 实例已使用次数。 当 返回 reject 时 表示实例不可用
-    //     return new Promise((resolve,reject) => {
-    //         if(maxCrawlerCount <= 0 || browser.useCount > maxCrawlerCount){
-    //             reject(false);
-
-    //         }else{
-    //             resolve(true);
-    //         }
-    //     })
-    // }
+			BROWSER_ARRAY[i] = browser;
+		}
+		console.log(BROWSER_ARRAY);
+	})();	
 }
-const config = {
-    max: MAX_BROWSER,
-    min: MAX_BROWSER,
-    testOnBorrow: false,
-    acquireTimeoutMillis: 30000,
-    fifo: false,
-    autostart: true,
-    evictionRunIntervalMillis: 500000,
-    idleTimeoutMillis: 3600000
-}
-
-const pool = genericPool.createPool(factory, config)
-// const genericAcquire = pool.acquire.bind(pool)
-
-// const count = function(browser){
-//     browser.useCount += 1
-// }
-// pool.acquire = () =>
-//     genericAcquire().then(instance => {
-//       instance.useCount += 1
-//       return instance
-// })
-// pool.use = fn => {
-//     let resource
-//     return pool.acquire().then(r => {
-//         resource = r
-//         resource.useCount += 1
-//         pool.release(resource)
-//         return resource
-//     }).then(fn).then(result => {
-//         // 不管业务方使用实例成功与后都表示一下实例消费完成
-//         console.log("resource.useCount",resource.useCount)
-//         if(resource.useCount >= maxCrawlerCount){
-//             console.log("pool.size",pool.size)
-//             if(pool.size >= 1){
-//                 pool.destroy(resource)
-//             }
-//         }
-//         return result
-//     }, err => {
-//         throw err
-//     }
-//     )
-// }
-
-pool.on('factoryCreateError', function (err) {
-    errorLog.error('factoryCreateError:', err);
-})
-
-pool.on('factoryDestroyError', function (err) {
-    errorLog.error('factoryDestroyError:', err);
-})
-// Destroying the pool:
-// pool.drain().then(() => pool.clear())
 
 startSchedule();//开启定时任务
 
@@ -101,7 +30,30 @@ function startSchedule() {
     schedule.scheduleJob('0 * * * * *', function () {
         //定时执行的代码
         infoLog.info("服务器进程存在...", dateUtil.formatDate(new Date()));
+        for(var i = 0;i < MAX_BROWSER;i++){
+            let browser =  BROWSER_ARRAY[i] ;
+            if(browser.useCount >= appConfig.maxCrawlerCount){
+                puppeteer.launch(launchOptions).then(newBrowser => {
+                    infoLog.info("browser create",newBrowser.wsEndpoint());
+                    newBrowser.useCount = 0
+                    BROWSER_ARRAY.splice(i,1,newBrowser);
+                });
+                
+            }
+		}
     });
 }
-
-module.exports = pool;
+const browserPool = {};
+browserPool.acquire = function(){
+    return new Promise((resolve,reject) => {
+        let browser = BROWSER_ARRAY.pop();
+        
+        if(browser){
+            BROWSER_ARRAY.push(browser)
+            resolve(browser);
+        }else{
+            reject("获取到的浏览器为空");
+        }
+    })
+}
+module.exports = browserPool;
--- public/javascripts/puppeteerCrawler.js
+++ public/javascripts/puppeteerCrawler.js
@@ -3,93 +3,69 @@
 const broswerPool = require('./browserManager');
 const log4js = require('./util/log4jUtil');
 const errorLog = log4js.getLogger('errorLog');
-const appConfig = require('../../config/config');
 
 puppeteerCrawler = {
     crawlerPage: function (url) {
         return new Promise((resolve) => {
             broswerPool.acquire().then(browser => {
                 browser.useCount += 1;
-                if (browser.useCount >= appConfig.maxCrawlerCount) {
-                    browser.newPage().then(page => {
-                        page.setRequestInterception(true).then(() => {
-                            page.on('request', interceptedRequest => {
-                                // if (interceptedRequest.url().endsWith('.js') || interceptedRequest.url().endsWith('.png') || interceptedRequest.url().endsWith('.jpg') || /* interceptedRequest.url().indexOf('login') > -1 || */ interceptedRequest.url().endsWith('.gif') || interceptedRequest.url().endsWith('.mp4') || interceptedRequest.url().endsWith('.svg') || interceptedRequest.url().endsWith('.jpeg')
-                                //     || interceptedRequest.url().endsWith('.webm') || interceptedRequest.url().endsWith('.ogg') || interceptedRequest.url().endsWith('.mp3') || interceptedRequest.url().endsWith('.wav') || interceptedRequest.url().endsWith('.flac') || interceptedRequest.url().endsWith('.aac')
-                                //     || interceptedRequest.url().endsWith('.woff') || interceptedRequest.url().endsWith('.eot') || interceptedRequest.url().endsWith('.ttf') || interceptedRequest.url().endsWith('.otf')
-                                // )
-                                if (interceptedRequest.resourceType === 'image' || interceptedRequest.resourceType === 'font' || interceptedRequest.resourceType === 'script' || interceptedRequest.resourceType === 'media')
-                                    interceptedRequest.abort();
-                                else
-                                    interceptedRequest.continue();
-                            });
-                            page.goto(url, {
-                                timeout: 10000,
-                                waitUntil: ['domcontentloaded']
-                            }).then(() => {
-                                page.content().then(content => {
-                                    if (!page.isClosed()) {
-                                        page.close();
-                                    }
-                                    if (content) {
-                                        resolve({ isSuccess: true, message: content });
-                                    }
-                                    else {
-                                        resolve({ isSuccess: false, message: "爬到的网页内容为空" });
-                                    }
-                                    broswerPool.destroy(browser)
-                                });
-                            }).catch(error => {
-                                errorLog.error("TimeoutError: Navigation timeout of 10000 ms exceeded", error);
+                browser.newPage().then(page => {
+                    page.setRequestInterception(true).then(() => {
+                        page.on('request', interceptedRequest => {
+                            if (interceptedRequest.url().endsWith('.js') || 
+                                interceptedRequest.url().endsWith('.png') || 
+                                interceptedRequest.url().endsWith('.jpg') || 
+                                interceptedRequest.url().endsWith('.webp') || 
+                                interceptedRequest.url().endsWith('.ico') || 
+                                /* interceptedRequest.url().indexOf('login') > -1 || */ 
+                                interceptedRequest.url().endsWith('.gif') || 
+                                interceptedRequest.url().endsWith('.mp4') || 
+                                interceptedRequest.url().endsWith('.svg') || 
+                                interceptedRequest.url().endsWith('.jpeg')
+                                || interceptedRequest.url().endsWith('.webm') || 
+                                interceptedRequest.url().endsWith('.ogg') || 
+                                interceptedRequest.url().endsWith('.mp3') || 
+                                interceptedRequest.url().endsWith('.wav') || 
+                                interceptedRequest.url().endsWith('.flac') || 
+                                interceptedRequest.url().endsWith('.aac') || 
+                                interceptedRequest.url().endsWith('.woff') || 
+                                interceptedRequest.url().endsWith('.eot') || 
+                                interceptedRequest.url().endsWith('.ttf') || 
+                                interceptedRequest.url().endsWith('.otf')
+                            )
+                            // if (interceptedRequest.resourceType === 'image' || interceptedRequest.resourceType === 'font' || interceptedRequest.resourceType === 'script' || interceptedRequest.resourceType === 'media')
+                                interceptedRequest.abort();
+                            else
+                                interceptedRequest.continue();
+                        });
+                        page.goto(url, {
+                            timeout: 10000,
+                            waitUntil: ['domcontentloaded']
+                        }).then(() => {
+                            page.content().then(content => {
                                 if (!page.isClosed()) {
                                     page.close();
                                 }
-                                broswerPool.destroy(browser)
-                                resolve({ isSuccess: false, message: JSON.stringify(error) });
-                            });
-                        });
-                    });
-                } else {
-                    broswerPool.release(browser);
-                    browser.newPage().then(page => {
-                        page.setRequestInterception(true).then(() => {
-                            page.on('request', interceptedRequest => {
-                                // if (interceptedRequest.url().endsWith('.js') || interceptedRequest.url().endsWith('.png') || interceptedRequest.url().endsWith('.jpg') || /* interceptedRequest.url().indexOf('login') > -1 || */ interceptedRequest.url().endsWith('.gif') || interceptedRequest.url().endsWith('.mp4') || interceptedRequest.url().endsWith('.svg') || interceptedRequest.url().endsWith('.jpeg')
-                                //     || interceptedRequest.url().endsWith('.webm') || interceptedRequest.url().endsWith('.ogg') || interceptedRequest.url().endsWith('.mp3') || interceptedRequest.url().endsWith('.wav') || interceptedRequest.url().endsWith('.flac') || interceptedRequest.url().endsWith('.aac')
-                                //     || interceptedRequest.url().endsWith('.woff') || interceptedRequest.url().endsWith('.eot') || interceptedRequest.url().endsWith('.ttf') || interceptedRequest.url().endsWith('.otf')
-                                // )
-                                if (interceptedRequest.resourceType === 'image' || interceptedRequest.resourceType === 'font' || interceptedRequest.resourceType === 'script' || interceptedRequest.resourceType === 'media')
-                                    interceptedRequest.abort();
-                                else
-                                    interceptedRequest.continue();
-                            });
-                            page.goto(url, {
-                                timeout: 10000,
-                                waitUntil: ['domcontentloaded']
-                            }).then(() => {
-                                page.content().then(content => {
-                                    if (!page.isClosed()) {
-                                        page.close();
-                                    }
-                                    if (content) {
-                                        resolve({ isSuccess: true, message: content });
-                                    }
-                                    else {
-                                        resolve({ isSuccess: false, message: "爬到的网页内容为空" });
-                                    }
-
-                                });
-                            }).catch(error => {
-                                errorLog.error("TimeoutError: Navigation timeout of 10000 ms exceeded", error);
-                                if (!page.isClosed()) {
-                                    page.close();
+                                if (content) {
+                                    resolve({ isSuccess: true, message: content });
                                 }
-                                resolve({ isSuccess: false, message: JSON.stringify(error) });
+                                else {
+                                    resolve({ isSuccess: false, message: "爬到的网页内容为空" });
+                                }
+
                             });
+                        }).catch(error => {
+                            errorLog.error("TimeoutError: Navigation timeout of 10000 ms exceeded", error);
+                            if (!page.isClosed()) {
+                                page.close();
+                            }
+                            resolve({ isSuccess: false, message: JSON.stringify(error) });
                         });
                     });
-                }
+                });
 
+            }).catch(error => {
+                resolve({ isSuccess: false, message: JSON.stringify(error) });
             })
 
         })
